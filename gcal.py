2a9a24cbf3f242dc5b221ab15e59ee93cfcd29ed
Jacob <jacob.m.chen.22@dartmouth.edu>
2020-10-26 02:41:54 -0400

    refactored token.pickle and removed API Data directory because unnecessary and causing problems for user

--- gcal.py
+++ gcal.py
@@ -39,8 +39,8 @@ class Gcal:
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists('API Data/token.pickle'):
-            with open('API Data/token.pickle', 'rb') as token:
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -58,7 +58,7 @@ class Gcal:
                 flow = InstalledAppFlow.from_client_config(client_config=clientcfg, scopes=SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('API Data/token.pickle', 'wb') as token:
+            with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
 
         # Log creds with self variable
